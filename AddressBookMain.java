d6f77c6a51e4c4cfff1ab2f568649f850757dc4b
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-05 01:01:59 +0530

    Added: Welcome to Address Book Program

---
+++ AddressBookMain.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class AddressBookMain {
+	public static void main(String []args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
