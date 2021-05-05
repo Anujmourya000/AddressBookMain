25754e32ef2d066000d0eb37ab9c98fca644e420
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-05 20:20:38 +0530

    Added: adding new contact from console is added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
 public class AddressBookMain {
-
     private String firstname;
     private String lastname;
     private String address;
     private String city;
     private String state;
-    private String zip;
+    private int zip;
     private String email;
-
-    public AddressBookMain(String firstname, String lastname, String address, String city, String state, String zip, String email){
+    /**
+     *  @description Constructor for AddressBookMain Class
+     *  @param
+     *
+     */
+    public AddressBookMain(String firstname, String lastname, String address, String city, String state, int zip, String email){
         this.firstname = firstname;
         this.lastname = lastname;
         this.address = address;
@@ -17,7 +21,11 @@ public class AddressBookMain {
         this.zip = zip;
         this.email = email;
     }
-
+    /**
+     *  @description Overriding toString Method to print the values of Heap memory instead of address
+     *  @param
+     *
+     */
     @Override
     public String toString() {
         return "AddressBookMain{" +
@@ -32,9 +40,15 @@ public class AddressBookMain {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address Book Program");
-        AddressBookMain Contacts = new AddressBookMain("Anuj", "Mourya", "Yugantar colony", "Bhopal",
-                                                        "M.P.", "462022", "akm123@gmail.com");
-        System.out.println("Contact Details are: "+Contacts);
+        AddressBookMain Contact1 = new AddressBookMain("Anuj", "Mourya", "Yugantar colony", "Bhopal",
+                                                        "M.P.", 462022, "akm123@gmail.com");
+        System.out.println("Contact1 Details are: "+Contact1);
+        System.out.println("please enter the contact details as:\n firstname\n lastname\n address\n city\n state\n zip\n email\n");
+        AddressBookMain Contact2 = new AddressBookMain(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(),
+                                                        sc.nextLine(), sc.nextInt(), sc.nextLine());
+
+        System.out.println("Contact2 Details are: "+Contact2);
     }
 }
